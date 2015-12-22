231278645f3d016b0af7d3100cc637063ea0c514
Thomas Haden <thomashaden@gmail.com>
2015-12-22 11:09:07 +0100

    Fixed 1GBit to 100MBit Connection

--- work/work/software/src/tse_tutorial.c
+++ work/work/software/src/tse_tutorial.c
@@ -54,7 +54,7 @@ int main(void)
 	while ( IORD_32DIRECT(TSE_BASE, 0x80*4) & 0x8000);	 
 	 
 	// Enable read and write transfers, gigabit Ethernet operation, and CRC forwarding
-	IOWR_ALTERA_TSEMAC_CMD_CONFIG(TSE_BASE, IORD_ALTERA_TSEMAC_CMD_CONFIG(TSE_BASE) | 0x0000004B);
+	IOWR_ALTERA_TSEMAC_CMD_CONFIG(TSE_BASE, IORD_ALTERA_TSEMAC_CMD_CONFIG(TSE_BASE) | 0x00000042);
 	
 	// Initialize display
 	display_init();
